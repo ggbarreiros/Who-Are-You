c2a309d0dab9ccfb98d88cc19524be7c326b5e46
ggbarreiros <ggbarreiros@hotmail.com>
2020-06-19 11:03:47 -0300

    WAYe
    Things could messed up real fast now! Changing sprites

---
+++ WAYe/Who Are You/Assets/_Scripts/Character/IdleAnimation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAnimation : MonoBehaviour
+{
+    public float timer = 0;
+    public float animationTime;
+    private bool animationPlaying = false;
+    private Animator anim;
+    private PlayerMovement playerMove;
+    private AttackSystemRequiem atkSystem;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMove = GetComponent<PlayerMovement>();
+        atkSystem = GetComponent<AttackSystemRequiem>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > animationTime && !animationPlaying)
+        {
+            //Fazer animação de Idle
+            anim.SetFloat("direction", 1f);
+            anim.Play("WaitAnimStart");
+            animationPlaying = true;
+            DeactivateScripts();
+        }
+
+        if (Input.anyKey && animationPlaying)
+        {
+            timer = 0;
+            animationPlaying = false;
+            anim.SetFloat("direction", -1f);
+        }
+        else if(Input.anyKey && !animationPlaying)
+        {
+            timer = 0;
+        }
+        
+    }
+
+    public void DeactivateScripts()
+    {
+        playerMove.enabled = false;
+        atkSystem.enabled = false;
+    }
+
+    public void ActivateScripts()
+    {
+        playerMove.enabled = true;
+        atkSystem.enabled = true;
+    }
+}
---
+++ WAYe/Who Are You/Assets/_Scripts/Enemy/FlipEnemy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipEnemy : MonoBehaviour
+{
+    public bool aware = false;
+    public GameObject player;
+    public bool facingRight = true;
+    public float distance;
+    private Animator anim;
+    public float speed;
+    //private Enemy enemy;
+    public bool ableToPursuit = true;
+
+    public bool attackDistance = false;
+
+    public MusicManager mManager;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        distance = Vector2.Distance(player.transform.position, transform.position);
+
+        if(distance < 14.4f)
+        {
+            
+            if (distance > 3f && ableToPursuit)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed);
+                aware = true;
+                //mManager.awarePlay = true;
+                anim.SetBool("isFollowing", true);
+                
+            }
+            else
+            {
+                anim.SetBool("isFollowing", false);
+            }
+        }
+        else
+        {
+            aware = false;
+            //mManager.awarePlay = false;
+            anim.SetBool("isFollowing", false);
+        }
+
+        if (aware)
+        {
+            mManager.awarePlay = true;
+        }
+        else
+        {
+            mManager.awarePlay = false;
+        }
+
+        if(distance < 6f)
+        {
+            attackDistance = true;
+            mManager.fightPlay = true;
+        }
+        else if(distance > 6f)
+        {
+            mManager.fightPlay = false;
+            attackDistance = false;
+        }
+
+        if (aware)
+        {
+            if (player.transform.position.x > transform.position.x && !facingRight)
+            {
+                Flip();
+            }
+            else if (player.transform.position.x < transform.position.x && facingRight)
+            {
+                Flip();
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    public void StopSounds()
+    {
+        mManager.awarePlay = false;
+        mManager.fightPlay = false;
+    }
+}
